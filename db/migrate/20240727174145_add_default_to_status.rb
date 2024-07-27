0c8d9ff165e509c063a037e49d9e0b792aa430ea
Haidamac <serge.krinitsa@gmail.com>
2024-07-28 00:45:54 +0300

    add migrations

---
+++ db/migrate/20240727174145_add_default_to_status.rb
@@ -0,0 +1,5 @@
+class AddDefaultToStatus < ActiveRecord::Migration[7.1]
+  def change
+    change_column :cars, :status, :integer, default: 0
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,47 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_07_27_174145) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "brand"
+    t.string "car_model"
+    t.string "body"
+    t.decimal "mileage"
+    t.string "color"
+    t.decimal "price"
+    t.string "fuel"
+    t.integer "year"
+    t.decimal "volume"
+    t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "role", default: 0
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+  end
+
+  add_foreign_key "cars", "users"
+end
